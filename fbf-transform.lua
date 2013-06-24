a002bcc1c6b7913d9e4f85229accb600637bde34
Lyger <lygerzero0zero@gmail.com>
2013-06-24 00:37:06 -0400

    Fixed position handling
    Copied position function from gradient-everything

--- fbf-transform.lua
+++ fbf-transform.lua
@@ -76,7 +76,7 @@ debug presets features
 
 script_name="Frame-by-frame transform"
 script_description="Smoothly transforms between the first and last selected lines."
-script_version="0.8.2"
+script_version="0.8.3"
 
 include("karaskel.lua")
 include("utils.lua")
@@ -581,8 +581,53 @@ local function get_pos(line)
 	if posx==nil then
 		_,_,posx,posy=line.text:find("\\move%(([%d%.%-]*),([%d%.%-]*),")
 		if posx==nil then
-			posx=line.x
-			posy=line.y
+			_,_,align_n=line.text:find("\\an([%d%.%-]*)")
+			if align_n==nil then
+				_,_,align_dumb=line.text:find("\\a([%d%.%-]*)")
+				if align_dumb==nil then
+					--If the line has no alignment tags
+					posx=line.x
+					posy=line.y
+				else
+					--If the line has the \a alignment tag
+					vid_x,vid_y=aegisub.video_size()
+					align_dumb=tonumber(align_dumb)
+					if align_dumb>8 then
+						posy=vid_y/2
+					elseif align_dumb>4 then
+						posy=line.eff_margin_t
+					else
+						posy=vid_y-line.eff_margin_b
+					end
+					_temp=align_dumb%4
+					if _temp==1 then
+						posx=line.eff_margin_l
+					elseif _temp==2 then
+						posx=line.eff_margin_l+(vid_x-line.eff_margin_l-line.eff_margin_r)/2
+					else
+						posx=vid_x-line.eff_margin_r
+					end
+				end
+			else
+				--If the line has the \an alignment tag
+				vid_x,vid_y=aegisub.video_size()
+				align_n=tonumber(align_n)
+				_temp=align_n%3
+				if align_n>6 then
+					posy=line.eff_margin_t
+				elseif align_n>3 then
+					posy=vid_y/2
+				else
+					posy=vid_y-line.eff_margin_b
+				end
+				if _temp==1 then
+					posx=line.eff_margin_l
+				elseif _temp==2 then
+					posx=line.eff_margin_l+(vid_x-line.eff_margin_l-line.eff_margin_r)/2
+				else
+					posx=vid_x-line.eff_margin_r
+				end
+			end
 		end
 	end
 	return posx,posy
@@ -686,17 +731,21 @@ function frame_transform(sub,sel,config)
 	local end_table={}
 	
 	--Separate each line into a table of tags and text
+	first_line.text=first_line.text:gsub("}","}\t")
 	x=1
 	for thistag,thistext in first_line.text:gmatch("({[^{}]*})([^{}]*)") do
-		start_table[x]={tag=thistag,text=thistext}
+		start_table[x]={tag=thistag,text=thistext:gsub("^\t","")}
 		x=x+1
 	end
+	first_line.text=first_line.text:gsub("}\t","}")
 	
+	last_line.text=last_line.text:gsub("}","}\t")
 	x=1
 	for thistag,thistext in last_line.text:gmatch("({[^{}]*})([^{}]*)") do
-		end_table[x]={tag=thistag,text=thistext}
+		end_table[x]={tag=thistag,text=thistext:gsub("^\t","")}
 		x=x+1
 	end
+	last_line.text=last_line.text:gsub("}\t","}")
 	
 	--Make sure both lines have the same splits
 	start_table,end_table=match_splits(start_table,end_table)
@@ -799,11 +848,13 @@ function frame_transform(sub,sel,config)
 		
 		--Break the line into a table
 		local this_table={}
+		this_line.text=this_line.text:gsub("}","}\t")
 		x=1
 		for thistag,thistext in this_line.text:gmatch("({[^{}]*})([^{}]*)") do
-			this_table[x]={tag=thistag,text=thistext}
+			this_table[x]={tag=thistag,text=thistext:gsub("^\t","")}
 			x=x+1
 		end
+		this_line.text=this_line.text:gsub("}\t","}")
 		
 		--Make sure it has the same splits
 		local j=1
@@ -1128,6 +1179,7 @@ function load_fbf(sub,sel)
 		
 		--Set undo point
 		aegisub.set_undo_point(script_name);
+		return sel
 	end
 end
 
