dbeec3cb2de28acf1575dbb30fcbaf8c8c1bd246
Lyger <lygerzero0zero@gmail.com>
2013-06-25 20:30:20 -0400

    Added advanced position handling
    Probably gonna keep tweaking this for a bit.

--- move-clip.lua
+++ move-clip.lua
@@ -11,9 +11,11 @@ to be present in selected line(s) in order to work.
 
 ]]
 
+include("karaskel.lua")
+
 script_name="Move with clip"
 script_description="Moves both position and rectangular clip"
-script_version="1.0"
+script_version="1.1"
 
 config=
 {
@@ -26,27 +28,93 @@ config=
 --Convert float to neatly formatted string
 local function f2s(f) return string.format("%.3f",f):gsub("%.(%d-)0+$","%.%1"):gsub("%.$","") end
 
+
+--Returns the position of a line
+local function get_pos(line)
+	local _,_,posx,posy=line.text:find("\\pos%(([%d%.%-]*),([%d%.%-]*)%)")
+	if posx==nil then
+		_,_,posx,posy=line.text:find("\\move%(([%d%.%-]*),([%d%.%-]*),")
+		if posx==nil then
+			_,_,align_n=line.text:find("\\an([%d%.%-]*)")
+			if align_n==nil then
+				_,_,align_dumb=line.text:find("\\a([%d%.%-]*)")
+				if align_dumb==nil then
+					--If the line has no alignment tags
+					posx=line.x
+					posy=line.y
+				else
+					--If the line has the \a alignment tag
+					vid_x,vid_y=aegisub.video_size()
+					align_dumb=tonumber(align_dumb)
+					if align_dumb>8 then
+						posy=vid_y/2
+					elseif align_dumb>4 then
+						posy=line.eff_margin_t
+					else
+						posy=vid_y-line.eff_margin_b
+					end
+					_temp=align_dumb%4
+					if _temp==1 then
+						posx=line.eff_margin_l
+					elseif _temp==2 then
+						posx=line.eff_margin_l+(vid_x-line.eff_margin_l-line.eff_margin_r)/2
+					else
+						posx=vid_x-line.eff_margin_r
+					end
+				end
+			else
+				--If the line has the \an alignment tag
+				vid_x,vid_y=aegisub.video_size()
+				align_n=tonumber(align_n)
+				_temp=align_n%3
+				if align_n>6 then
+					posy=line.eff_margin_t
+				elseif align_n>3 then
+					posy=vid_y/2
+				else
+					posy=vid_y-line.eff_margin_b
+				end
+				if _temp==1 then
+					posx=line.eff_margin_l
+				elseif _temp==2 then
+					posx=line.eff_margin_l+(vid_x-line.eff_margin_l-line.eff_margin_r)/2
+				else
+					posx=vid_x-line.eff_margin_r
+				end
+			end
+		end
+	end
+	return posx,posy
+end
+
 function move_clip(sub,sel)
 	pressed,results=aegisub.dialog.display(config,{"Move","Cancel"})
 	
 	d_x=results["d_x"]
 	d_y=results["d_y"]
 	
+	meta,styles=karaskel.collect_head(sub,false)
+	
 	for _,li in ipairs(sel) do
 		line=sub[li]
 		
-		if line.text:match("\\clip%([%d%-%.]+,[%d%-%.]+,[%d%-%.]+,[%d%-%.]+%)")~= nil
-			and line.text:match("\\pos%([%d%-%.]+,[%d%-%.]+%)")~= nil then
+		if line.text:match("\\clip%([%d%-%.]+,[%d%-%.]+,[%d%-%.]+,[%d%-%.]+%)")~= nil then
+			
+			karaskel.preproc_line(sub,meta,styles,line)
 			
 			dur=line.end_time-line.start_time
 			
-			line.text=line.text:gsub("\\pos%(([%d%-%.]+),([%d%-%.]+)%)",
-				function(x1,y1)
-					x1=tonumber(x1)
-					y1=tonumber(y1)
-					return string.format("\\move(%s,%s,%s,%s)",
-						f2s(x1),f2s(y1),f2s(x1+d_x),f2s(y1+d_y))
-				end)
+			ox,oy=get_pos(line)
+			
+			line.text=line.text:gsub("\\pos%([%d%-%.]+,[%d%-%.]+%)","")
+			line.text=line.text:gsub("\\move%([%d%-%.,]+%)","")
+			line.text=line.text:gsub("{",
+				function()
+					x1=tonumber(ox)
+					y1=tonumber(oy)
+					return string.format("{\\move(%s,%s,%s,%s,%d,%d)",
+						f2s(x1),f2s(y1),f2s(x1+d_x),f2s(y1+d_y),0,dur)
+				end,1)
 			
 			line.text=line.text:gsub("\\clip%(([%d%-%.]+),([%d%-%.]+),([%d%-%.]+),([%d%-%.]+)%)",
 				function(x1,y1,x2,y2)
